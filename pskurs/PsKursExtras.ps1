c8d2359e3c11730c2891c14672143e2e8e3e08f9
Peter Monadjemi <pm@activetraining.de>
2017-09-17 15:13:24 +0200

    PsKurs-Modul hinzugefügt

---
+++ pskurs/PsKursExtras.ps1
@@ -0,0 +1,16 @@
+﻿<#
+ .Synopsis
+ Liefert allgemeine Konfigurationsdaten über den lokalen Computer
+#>
+function Get-PCInfo
+{
+    $OS = Get-OSInfo
+    $Computer = Get-ComputerInfo
+    [PSCustomObject]@{
+        Name = $Computer.Name
+        OSName = $OS.Caption
+        OSVersion = $OS.Version
+        PCHersteller = $Computer.Manufacturer
+    }
+
+}
---
+++ pskurs/Pskurs.psm1
@@ -0,0 +1,31 @@
+﻿<#
+ .Synopsis
+ Liefert allgemeine Konfigurationsdaten über die Hardware
+#>
+
+function Get-ComputerInfo
+{
+    Get-CimInstance -ClassName Win32_ComputerSystem | Select Name, Manufacturer, Model, SystemType, TotalPhysicalMemory
+}
+
+<#
+ .Synopsis
+ Liefert allgemeine Konfigurationsdaten über das Betriebssystem
+#>
+function Get-OSInfo
+{
+    Get-CimInstance -ClassName Win32_OperatingSystem | Select Caption, Version, LastBootUpTime
+}
+
+<#
+ .Synopsis
+ Liefert allgemeine Konfigurationsdaten über die installierten Anwendungen
+#>
+
+function Get-AppInfo
+{
+    $RegPfad1 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    $RegPfad2 = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    Get-ItemProperty -Path $RegPfad1, $RegPfad2 | Select-Object DisplayName, DisplayVersion, Publisher
+}
+
