0fa896837dcf96cec7325bb1b3b8e276361ad368
Peter Monadjemi <pm@activetraining.de>
2017-09-17 14:10:07 +0200

    GetStorageCost

---
+++ GetStorageCostV1.ps1
@@ -0,0 +1,15 @@
+﻿
+function Get-StorageCost
+{
+    <#
+     .Synopsis
+     Berechnet die Speicherkosten für einen Verzeichnisinhalt
+    #>
+    [CmdletBinding()]
+    param([Parameter(Mandatory=$true)][String]$Path,
+          [Double]$CostMB=0.02)
+
+    $SizeMB = (Get-ChildItem -Path $Path -File -ErrorAction Ignore | Measure-Object -Property Length -Sum).Sum / 1MB
+    return [Math]::Round($SizeMB * $CostMB, 2)
+
+}
