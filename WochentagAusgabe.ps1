d92f2df8b24e63b8cd83de47226023da4ee81ea0
Peter Monadjemi <pm@activetraining.de>
2017-09-17 14:29:23 +0200

    WochentagAusgabe hinzugefügt

---
+++ WochentagAusgabe.ps1
@@ -0,0 +1,13 @@
+﻿<#
+ .Synopsis
+  Wochentagausgabe
+ #>
+
+ $Wochentag = @{
+    n="Wochentag"
+    e= { "Montag" }
+ }
+
+ dir -Path C:\Windows | Select-Object Name, CreationTime, $Wochentag
+
+ # Wichtig: Anstelle von "Montag" soll natürlich der richtige Wochentag ausgegeben werden
