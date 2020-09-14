d2ee2c70ebcbb64e43d8ee627b02b854f853471a
Peter Monadjemi <pm@eureka-fach.de>
2020-09-14 14:25:36 +0200

    Material-Ordner angelegt

--- TB03_PipelineParameterBeispiel.ps1
+++ TB03_PipelineParameterBeispiel.ps1
@@ -1,22 +1,26 @@
 <#
  .Synopsis
- Ein Beispiel f√ºr den Pipeline-Parameter
+ Ein Beispiel f¸r den Pipeline-Parameter
 #>
 
-# Ein sehr einfaches Beispiel f√ºr das generelle Verst√§ndnis
+<#
+
+# Ein sehr einfaches Beispiel f¸r das generelle Verst‰ndnis
 
 (1..10) | Where-Object -PipelineVariable z { $_ % 2 -eq 1} | ForEach-Object {
-    "$z wurde gepr√ºft"
+    "$z wurde gepr¸ft"
 }
 
 Get-Process -PipelineVariable p | Select-Object StartTime -First 3 | ForEach-Object {
     "Prozess: $($p.Name) startete um $($_.StartTime)"
 }
 
+#>
+
 # Verzeichnisse ausgeben, auf die ein Benutzerkonto Vollzugriff besitzt,
 # das kein Admin ist
-Get-ChildItem -Path C:\ -ErrorAction Ignore -Directory -PipelineVariable File |
+Get-ChildItem -Path C:\ -ErrorAction Ignore -Directory -PipelineVariable dir |
   Get-ACL | Select-Object -ExpandProperty Access |
   Where-Object { $_.FileSystemRights -eq "FullControl" -and  `
-   $_.IdentityReference -notmatch "Administratoren"} |
-   Select-Object -Property IdentityReference,  @{n="Path";e={$File.FullName } }
+   $_.IdentityReference -notmatch "Administrator"} | 
+    Select-Object -Property @{n="Benutzerkonto";e={$_.IdentityReference}}, @{n="Verzeichnis";e={$dir.FullName } }
