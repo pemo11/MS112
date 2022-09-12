4b07a77e4746b1f3a16b6665e61003dc483541c9
Peter Monadjemi <pm@activetraining.de>
2022-09-12 16:20:23 +0200

    Passwort im Klartext.-Kommentar

--- Vorbereitung/FtpDownload.ps1
+++ Vorbereitung/FtpDownload.ps1
@@ -6,6 +6,7 @@
 $FtpServer = "ftp://wp12146773.server-he.de"
 $FileName = "posh/MS112.zip"
 $FtpUsername = "ftp12146773-pskurs"
+# Passwort im Klartext-Alarm - in diesem Fall aber nicht tragisch (denke ich)
 $FtpPassword = "posh2021"
 
 $Cred = [PSCredential]::New($FtpUsername, ($FtpPassword | ConvertTo-SecureString -AsPlainText -Force))
