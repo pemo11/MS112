7eb4c0330abda1b872a543f0baccf12718b94267
Peter Monadjemi <pm@activetraining.de>
2022-10-19 09:30:28 +0200

    SSH-Beispiel überarbeitet

--- TBX2/StartSSH.ps1
+++ TBX2/StartSSH.ps1
@@ -5,8 +5,17 @@
 
 #requires -version 7.0
 
-$Hostname = "64.225.108.74"
+$Hostname = "165.227.148.64"
 $Username = "root"
 
-$S1 = New-PSSession -HostName $Hostname -UserName $Username -SSHTransport -Subsystem powershell
-Enter-PSSession –Session $S1
+# Gibt leider aktuell einen Permission Denied Fehler aus - PasswortAuthentifizierung is no, da ich den KeyFilePath Parameter verwende
+# Hat vielleicht tiefergehende Gründe, die ich nicht kenne
+
+# $S1 = New-PSSession -HostName $Hostname -UserName $Username -SSHTransport -Subsystem powershell
+$KeyPath = "C:\Users\pemo20\.ssh\id_ed25519"
+# SSHTransport ist überflüssig, wenn der Hostname-Parameter verwendet wird und Subsystem ist immer powershell
+$S1 = New-PSSession -HostName $Hostname -UserName $Username -KeyFilePath $KeyPath
+if ($null -ne $S1)
+{
+    Enter-PSSession –Session $S1
+}
