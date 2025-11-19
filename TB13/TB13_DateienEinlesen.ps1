ef301fbd2be954c07d2d8f792e4da5908873b687
Peter Monadjemi <pm@activetraining.de>
2025-11-19 10:50:26 +0100

    Aktueller Stand 19/11/2025

--- TB13/TB13_DateienEinlesen.ps1
+++ TB13/TB13_DateienEinlesen.ps1
@@ -17,7 +17,7 @@ using namespace System.Text
 $TxtPfad = [Path]::GetTempFileName()
 
 $Txt = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
-$Limit = 50MB
+$Limit = 100MB
 $Sb = [StringBuilder]::new()
 while($Sb.Length -lt $Limit)
 {
--- TB13/TB13_PipelineAbbrechen.ps1
+++ TB13/TB13_PipelineAbbrechen.ps1
@@ -5,4 +5,6 @@
 
 #requires -Modules @{ModuleName="poshkurs";RequiredVersion="1.2.0"}
 
-Get-Computerkonten -Anzahl 1000 | Select-Object -First 10
+Get-Computerkonten -Anzahl 10000 | Select-Object -First 10
+
+Get-Computerkonten -Anzahl 10000 | Select-Object -Last 10
