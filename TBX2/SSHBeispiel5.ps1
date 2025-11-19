4ba8cdd5b3e92e26ed1eea8966431334f975dda1
Peter Monadjemi <pm@activetraining.de>
2025-11-19 11:49:59 +0100

    SSH-Beispiel mit Return-Code

--- TBX2/SSHBeispiel5.ps1
+++ TBX2/SSHBeispiel5.ps1
@@ -13,12 +13,15 @@ $SB = {
     Write-Host "Prozesse mit weniger als $using:LimitKB KB Arbeitsspeicher:" -ForegroundColor Yellow
     Get-Process | Where-Object { ($_.WS / 1KB) -lt $using:LimitKB } |
       Format-Table Name, Id, @{Name="WorkingSet(KB)";Expression={ [Math]::Round($_.WS / 1KB, 2) }} -AutoSize
+    Return 1234
 }
 
 $Ps1Path = Join-Path -Path $PSScriptRoot -ChildPath "RemoteScriptParam.ps1"
 $Ps1Code | Out-File -FilePath $Ps1Path -Encoding UTF8
 
 $S1 = New-PSSession -HostName $IPAdresse -UserName $Username
-Invoke-Command -Session $S1 -ScriptBlock $SB
-
+$Result = Invoke-Command -Session $S1 -ScriptBlock $SB
+$RetCode = $Result[-1]
+Write-Host "Rückgabewert des Remote-Skripts: $RetCode"
+$Result[0..($Result.Count - 2)]  # Alle bis auf den letzten Eintrag anzeigen
 Get-PSSession | Remove-PSSession -Verbose
